fa2bec7a63b943b544e5f4647b06888724f204ad
hades <amir.m.343@outlook.com>
2023-08-12 14:08:52 +0330

    -001.0 example1: bell_state

--- bell_state.py
+++ bell_state.py
@@ -1,17 +1,21 @@
 from time import time
 
-import matmul
+import operators.matrix_multipication.matmul as matmul
+import operators.tensor_product.tensor_prod as tp
+
 import numpy as np
 
 bt = time()
 
-ket0 = np.array([[1], [0]], order='F')
-ket1 = np.array([[0], [1]], order='F')
+ket0 = np.array([[0], [1]], order='F')
+ket1 = np.array([[1], [0]], order='F')
 
 a = ket0
 b = ket1
 
-psi = fop.tensor_product.tp(a,b)
+psi = tp.tp(a,b)
+
+# print(psi)
 
 i = np.array([
     [1, 0],
@@ -23,9 +27,9 @@ hadamard = (1/np.sqrt(2))*np.array([
     [1, -1]
 ], order='F')
 
-hi = fop.tensor_product.tp(hadamard, i)
+hi = tp.tp(hadamard, i)
 
-psi = fop.matrix_multipication.matmul(hi, psi)
+psi = matmul.matmul(hi, psi)
 
 c_not = np.array([
     [1, 0, 0, 0],
@@ -34,7 +38,7 @@ c_not = np.array([
     [0, 0, 1, 0]
 ], order='F')
 
-psi = fop.matrix_multipication.matmul(c_not, psi)
+psi = matmul.matmul(c_not, psi)
 
 print(psi)
 
